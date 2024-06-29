530d4994bf60dfb5cee9596d650bc1f1910324c2
William Amaya <william.amaya@sykes.com>
2024-06-28 22:57:27 -0600

    commit inicial

---
+++ NamesSplit.Management/NamesSplit.Management/NamesSplitManagement.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace NamesSplit.Management
+{
+    public class NamesSplitManagement
+    {
+        public string[] GetNames(string fullname)
+        {
+            string[] compuestos = new string[] { "de" };
+            string name = string.Empty;
+            string[] names = fullname.Split(' ');
+
+            foreach (string item in names)
+            {
+                if (compuestos.Contains(item))
+                {
+                    name += $"{item}_";
+                }
+                else
+                {
+                    name += $"{item} ";
+                }
+            }
+
+            string[] fix = name.Split(' ').ToList().Select(x => x.Contains('_') ? x.Replace('_', ' ') : x).ToArray();
+
+            return fix;
+        }
+    }
+}
---
+++ NamesSplit.Management/NamesSplit.Management/README.md
@@ -0,0 +1,2 @@
+# NamesSplit.Management
+Proyecto en el cual se proveen metodos para separar nombres con o sin nombres y apellidos compuestos
